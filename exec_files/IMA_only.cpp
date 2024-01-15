218d5d9cbbab0f29466a2d408672edc173c3395d
chenxlong3 <1226608021@qq.com>
2024-01-15 11:10:34 +0800

    format func

--- exec_files/IMA_only.cpp
+++ exec_files/IMA_only.cpp
@@ -4,7 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+   // get mode
+   string func = "";
+   for (int i = 0; i < argc; i++) {
+      if (argv[i] == string("-func")) func = string(argv[i + 1]);
+   }
+   if (func == "format") {
+       format_graph(argc, argv);
+       return 1;
+   }
+   
    InfGraph g = parseArg(argc, argv);
    run_only_IMA(g);
-   return 0;
+   // run_only_AISPAC(g);
+   return 1;
 }
