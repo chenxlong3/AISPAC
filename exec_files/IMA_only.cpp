901f90875b5bab177f8e5d96a3d037a5c2e20b25
chenxlong3 <1226608021@qq.com>
2024-01-17 23:28:35 +0800

    args

--- exec_files/IMA_only.cpp
+++ exec_files/IMA_only.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-   // get mode
+   // if needing to 
    string func = "";
    for (int i = 0; i < argc; i++) {
       if (argv[i] == string("-func")) func = string(argv[i + 1]);
@@ -14,7 +14,11 @@ int main(int argc, char *argv[])
        return 1;
    }
    
-   InfGraph g = parseArg(argc, argv);
+   Argument args = parse_args(argc, argv);
+   dsfmt_gv_init_gen_rand(args.rand_seed);
+   InfGraph g(args.folder_name, args.graph_file, args.probability_mode);
+   g.set_args(args);    // setting arguments
+
    run_only_IMA(g);
    // run_only_AISPAC(g);
    return 1;
